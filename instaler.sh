7b66ccd7d9cd09213eba6e752c786a0470d55e48
MOBSAD <paulohenriquedlf@gmail.com>
2026-01-10 00:43:42 -0300

    melhorias no instalador e incrementação do .zshrc para "despoluir" o arquivo instaler.sh

--- instaler.sh
+++ instaler.sh
@@ -2,37 +2,59 @@
 set -e
 sudo -v
 
-sudo pacman -Syu python3 kitty zsh neovim git rclone man flameshot npm flatpak winetricks obs-studio zip unzip noto-fonts noto-fonts-emoji noto-fonts-extra discord pavucontrol steam fastfetch fakeroot --noconfirm
+#DEFINE SHELL COMO BASH
+if [ "$SHELL" != "$(which bash)" ]; then 
+  chsh -s "$(which bash)"
+fi
+
+#ATUALIZAÇÕES E INSTALAÇÕES GERAIS
+sudo pacman -Syu python3 kitty zsh neovim git rclone man flameshot npm flatpak winetricks obs-studio zip unzip noto-fonts noto-fonts-emoji wget noto-fonts-extra discord pavucontrol steam fastfetch fakeroot --noconfirm
 sudo pacman -S --needed flatpak
-flatpak install flathub com.spotify.Client
-flatpak install flathub org.vinegarhq.Sober
-flatpak install flathub com.github.tchx84.Flatseal
-flatpak install flathub org.kde.kdenlive
 
-ZSHWAYPATH="$HOME/.zshrc"
+#INSTALAÇÕES FLATHUB (SPOTIFY, SOBER [ROBLOX], FLATSEAL, KDENLIVE)
+flatpak install -y flathub \
+  com.spotify.Client \
+  org.vinegarhq.Sober \
+  com.github.tchx84.Flatseal \
+  org.kde.kdenlive
 
-add_alias(){
-  grep -qxf "$1" "$ZSHWAYPATH" || echo "$1" >> "$ZSHWAYPATH"
-}
+#SPOTIFY SCRIPT
+bash <(curl -sSL https://spotx-official.github.io/run.sh)>
 
-add_alias "alias std='shutdown now'"
-add_alias "alias rbt='reboot'"
-add_alias "alias vi='nvim'"
-add_alias "alias vim='nvim'"
-
-#spotify script
-bash <(curl -sSL https://spotx-official.github.io/run.sh)
-
-#instalando astronvim
+#INSTALANDO ASTRONVIM
 [ -d ~/.config/nvim ] && mv ~/.config/nvim ~/.config/nvim.bak
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim 
-# remove template's git connection to set up your own later
 rm -rf ~/.config/nvim/.git
 nvim
 
-rm -rf ~/yay
+#REMOVE PASTA DO YAY
+rm -rf $HOME/yay
+
+#DEFINE CAMINHO COMO /HOME/USUARIO
+cd $HOME
+
+#CLONA REPO DO YAY
 git clone https://aur.archlinux.org/yay.git
-cd ~/yay
-sudo pacman -S --needed base-devel
+
+#INSTALA DEPENDENCIAS
+sudo pacman -Syu --needed base-devel debugedit --noconfirm
+
+#ENTRA NA PASTA DO YAY
+cd yay
+
+#FAZ BUILD
 makepkg -si
+
+#AQUI TERÁ QUE ACEITAR MANUALMENTE NA INSTALAÇÃO GERAL
+#...
+#...
+
+#ATUALIZA O YAY
+yay -Syu --noconfirm
+
+#REMOVENDO POSSIVEL OHMYZSH EXISTENTE ANTES DE INSTALAR DENOVO
+rm -r /home/eomobbr/.oh-my-zsh
+
+#INSTALANDO OHMYZSH
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
