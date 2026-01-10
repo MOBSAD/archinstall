ed9585234688ad44cfe72b11b596ff0e67456440
MOBSAD <paulohenriquedlf@gmail.com>
2026-01-10 02:31:43 -0300

    Melhoria na legibilidade do código, correções e automação na auto-exclusão pós execução

--- instaler.sh
+++ instaler.sh
@@ -5,6 +5,7 @@ sudo -v
 #DEFINE SHELL COMO BASH
 if [ "$SHELL" != "$(which bash)" ]; then 
   chsh -s "$(which bash)"
+  sudo chsh -s "$(which bash)"
 fi
 
 #ATUALIZAÇÕES E INSTALAÇÕES GERAIS
@@ -19,16 +20,22 @@ flatpak install -y flathub \
   org.kde.kdenlive
 
 #SPOTIFY SCRIPT
-bash <(curl -sSL https://spotx-official.github.io/run.sh)>
+curl -sSL https://spotx-official.github.io/run.sh -o spotx.sh
+bash spotx.sh -f
+rm spotx.sh
+
+#REMOVENDO PASTA NVIM
+rm -rf "$HOME/.config/nvim"
 
 #INSTALANDO ASTRONVIM
-[ -d ~/.config/nvim ] && mv ~/.config/nvim ~/.config/nvim.bak
-git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim 
-rm -rf ~/.config/nvim/.git
-nvim
+git clone --depth 1 https://github.com/AstroNvim/template "$HOME/.config/nvim" 
+rm -rf "$HOME"/.config/nvim/.git
+#DEIXA PRA ABRIR DEPOIS E CONFIGURAR DEPOIS TAMBÉM
+#...
+#...
 
 #REMOVE PASTA DO YAY
-rm -rf $HOME/yay
+rm -rf "$HOME/yay"
 
 #DEFINE CAMINHO COMO /HOME/USUARIO
 cd $HOME
@@ -42,8 +49,8 @@ sudo pacman -Syu --needed base-devel debugedit --noconfirm
 #ENTRA NA PASTA DO YAY
 cd yay
 
-#FAZ BUILD
-makepkg -si
+#FAZ BUILD QUASE AUTOMATICAMENTE
+makepkg -si --noconfirm
 
 #AQUI TERÁ QUE ACEITAR MANUALMENTE NA INSTALAÇÃO GERAL
 #...
@@ -52,9 +59,43 @@ makepkg -si
 #ATUALIZA O YAY
 yay -Syu --noconfirm
 
-#REMOVENDO POSSIVEL OHMYZSH EXISTENTE ANTES DE INSTALAR DENOVO
-rm -r /home/eomobbr/.oh-my-zsh
+export CHSH=yes
+export RUNZSH=no
+
+#REMOVENDO POSSIVEL OHMYZSH E ARQUIVOS ZSH CASO EXISTENTES ANTES DE INSTALAR PARA EVITAR ERROS
+rm -rf "$HOME/.oh-my-zsh" "$HOME/.zshrc" "$HOME/.zsh_history" "$HOME/.shell.pre-oh-my-zsh"
 
 #INSTALANDO OHMYZSH
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+#FAZ BACKUP DO ARQUIVO ORIGINAL .ZSHRC
+mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
+
+#MOVE O ARQUIVO DO GITHUB PRA "SUBSTITUIR" O ORIGINAL
+mv "$HOME/archinstall/zshrc" "$HOME/.zshrc"
+
+#DEFINE SHELL COMO ZSH
+if [ "$SHELL" != "$(which zsh)" ]; then 
+  chsh -s "$(which zsh)"
+  sudo chsh -s "$(which zsh)"
+fi
+
+#CORES PRA FORMATAR
+RED='\033[0;31m'
+NC='\033[0m'
+
+#CONTAGEM REGRESSIVA PRA REINICIAR
+for i in {10..1}; do
+    echo -ne "Reiniciando em ${RED}$i${NC} segundos...\r"
+    sleep 1
+done
+echo ""
+
+#SE REMOVE
+rm -rf "$HOME/archinstall/"
+
+sync
+sleep 1
+
+#FAZ REBOOT
+reboot now
